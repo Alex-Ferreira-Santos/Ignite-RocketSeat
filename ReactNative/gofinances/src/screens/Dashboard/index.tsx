592a7aa44e9b1af02089dc0403bfc68487f85df5
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-07 11:49:07 -0300

    add icons

--- ReactNative/gofinances/src/screens/Dashboard/index.tsx
+++ ReactNative/gofinances/src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {Container, Title} from './styles'
+import {
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    Icon
+} from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri:'https://avatars.githubusercontent.com/u/64551315?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá</UserGreeting>
+                            <UserName>Alex</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name='power'/>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
