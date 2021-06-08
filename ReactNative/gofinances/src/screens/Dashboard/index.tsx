9c2f487f11f480809f2fa796f7d9e80aee88eb5e
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-08 09:58:37 -0300

    finished list transactions

--- ReactNative/gofinances/src/screens/Dashboard/index.tsx
+++ ReactNative/gofinances/src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HighLightCard } from '../../components/HighLightCard';
-import {TransactionCard} from '../../components/TransactionCard'
+import {TransactionCard,TransactionCardProps} from '../../components/TransactionCard'
 import {
     Container,
     Header,
@@ -17,8 +17,14 @@ import {
     TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+    id: string
+}
+
 export function Dashboard(){
-    const data = [{
+    const data: DataListProps[] = [{
+        id:'1',
+        type: 'positive',
         title:"Desenvolvimento de site",
         amount:"R$ 12.000,00",
         category:{
@@ -27,21 +33,25 @@ export function Dashboard(){
         },
         date:"13/04/2020"
     },{
-        title:"Desenvolvimento de site",
-        amount:"R$ 12.000,00",
+        id:'2',
+        type: 'negative',
+        title:"Hamburgueria Pizzy",
+        amount:"R$ 59,00",
         category:{
-            name: 'Vendas',
-            icon: 'dollar-sign'
+            name: 'Alimentação',
+            icon: 'coffee'
         },
-        date:"13/04/2020"
+        date:"10/04/2020"
     },{
-        title:"Desenvolvimento de site",
-        amount:"R$ 12.000,00",
+        id: '3',
+        type: 'negative',
+        title:"Aluguel do apartamento",
+        amount:"R$ 1.200,00",
         category:{
-            name: 'Vendas',
-            icon: 'dollar-sign'
+            name: 'Casa',
+            icon: 'shopping-bag'
         },
-        date:"13/04/2020"
+        date:"10/04/2020"
     }]
     return (
         <Container>
@@ -83,11 +93,8 @@ export function Dashboard(){
                 <Title>Listagem</Title>
                 <TransactionList
                     data={data}
+                    keyExtractor={item => item.id}
                     renderItem={({item}) => <TransactionCard data={item}/>}
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{
-                        paddingBottom: 10
-                    }}
                 >
                     
                 </TransactionList>
--- ReactNative/gofinances/src/screens/Dashboard/styles.ts
+++ ReactNative/gofinances/src/screens/Dashboard/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native'
+import {FlatList} from 'react-native'
 import {RFPercentage,RFValue} from 'react-native-responsive-fontsize'
 import {Feather} from '@expo/vector-icons'
+import {DataListProps} from '.'
 
 export const Container = styled.View`
     flex:1;
@@ -80,6 +82,13 @@ export const Title = styled.Text`
     margin-bottom: 16px;
 `
 
-export const TransactionList = styled.FlatList`
+export const TransactionList = styled(
+    FlatList as new () => FlatList<DataListProps>
+    ).attrs({
+    showsVerticalScrollIndicator:false,
+    contentContainerStyle:{
+        paddingBottom: 10
+    }
+})`
 
 `
