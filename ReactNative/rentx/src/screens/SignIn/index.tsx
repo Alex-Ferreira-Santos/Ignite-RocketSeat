7f023525778db7eb33dc6a300e158f8fa9be1ef9
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-28 17:50:34 -0300

    typing icons

--- ReactNative/rentx/src/screens/SignIn/index.tsx
+++ ReactNative/rentx/src/screens/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {StatusBar} from 'react-native'
-import { Container, Header, Title, Subtitle, Footer } from './styles';
+import { Container, Header, Title, Subtitle, Form, Footer } from './styles';
 
 import {Button} from '../../components/Button'
+import {Input} from '../../components/Input'
+
 import theme from '../../styles/theme';
 
 export function SignIn() {
@@ -13,6 +15,10 @@ export function SignIn() {
         <Title>Estamos{'\n'}quase lá</Title>
         <Subtitle>Faça seu login para começar{'\n'}uma experiência incrível</Subtitle>
       </Header>
+
+      <Form>
+        <Input iconName='mail'/>
+      </Form>
       <Footer>
         <Button 
           title='Login' 
