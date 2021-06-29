6bdf805777609dabb39f072c02d6b5bab2a135dd
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-06-29 14:17:29 -0300

    add api

--- ReactNative/rentx/src/routes/Stack.routes.tsx
+++ ReactNative/rentx/src/routes/Stack.routes.tsx
@@ -5,7 +5,7 @@ import {Home} from '../screens/Home'
 import {CarDetails} from '../screens/CarDetails'
 import {Scheduling} from '../screens/Scheduling'
 import {SchedulingDetails} from '../screens/SchedulingDetails'
-import {SchedulingComplete} from '../screens/SchedulingComplete'
+import {Confirmation} from '../screens/Confirmation'
 import {MyCars} from '../screens/MyCars';
 import {Splash} from '../screens/Splash';
 import {SignIn} from '../screens/SignIn'
@@ -24,7 +24,7 @@ export function StackRoutes(){
             <Screen name="carDetails" component={CarDetails}/>
             <Screen name="scheduling" component={Scheduling}/>
             <Screen name="schedulingDetails" component={SchedulingDetails}/>
-            <Screen name="schedulingComplete" component={SchedulingComplete}/>
+            <Screen name="Confirmation" component={Confirmation}/>
             <Screen name="myCars" component={MyCars}/>
         </Navigator>
     )
