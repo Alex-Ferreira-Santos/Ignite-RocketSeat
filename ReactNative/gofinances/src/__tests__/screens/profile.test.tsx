6ba977e7cbc0efee8c13bd028d6e158646027d63
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-02 10:16:28 -0300

    test suite

--- ReactNative/gofinances/src/__tests__/screens/profile.test.tsx
+++ ReactNative/gofinances/src/__tests__/screens/profile.test.tsx
@@ -3,26 +3,29 @@ import {render} from '@testing-library/react-native'
 
 import {Profile} from '../../screens/Profile'
 
-test('checks if show correctly user input name placeholder', () => {
-    const { getByPlaceholderText } = render(<Profile/>)
-    const inputName = getByPlaceholderText('nome')
-    
-    
-    expect(inputName).toBeTruthy()
+describe('Profile Screen', () => {
+    it('should have correctly placeholder in user name input ', () => {
+        const { getByPlaceholderText } = render(<Profile/>)
+        const inputName = getByPlaceholderText('nome')
+        
+        
+        expect(inputName).toBeTruthy()
+    })
+
+    it('should load user data',()=>{
+        const { getByTestId } = render(<Profile/>)
+        const inputName = getByTestId('input-name')
+        const inputSurname = getByTestId('input-surname')
+
+        expect(inputName.props.value).toEqual('Alex')
+        expect(inputSurname.props.value).toEqual('Ferreira')
+    })
+
+    it('should exists title correctly', () => {
+        const { getByTestId } = render(<Profile/>)
+        const textTitle = getByTestId('text-title')
+
+        expect(textTitle.props.children).toContain('Perfil')
+    })
 })
 
-test('checks if user data has benn loaded',()=>{
-    const { getByTestId } = render(<Profile/>)
-    const inputName = getByTestId('input-name')
-    const inputSurname = getByTestId('input-surname')
-
-    expect(inputName.props.value).toEqual('Alex')
-    expect(inputSurname.props.value).toEqual('Ferreira')
-})
-
-test('checks if title render correctly', () => {
-    const { getByTestId } = render(<Profile/>)
-    const textTitle = getByTestId('text-title')
-
-    expect(textTitle.props.children).toContain('Perfil')
-})
