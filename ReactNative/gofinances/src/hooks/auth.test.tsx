1017391432ec1b14be85eb648429f8b5ba23072a
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-02 13:26:40 -0300

    custom mocks

--- ReactNative/gofinances/src/hooks/auth.test.tsx
+++ ReactNative/gofinances/src/hooks/auth.test.tsx
@@ -1,25 +1,24 @@
 import { renderHook, act} from '@testing-library/react-hooks'
+import { mocked } from 'ts-jest/utils'
 import {AuthProvider,useAuth} from './auth'
+import {logInAsync} from 'expo-google-app-auth'
 
-jest.mock('expo-google-app-auth', ()=>{
-    return {
-        logInAsync: () => {
-            return{
-                type: 'success',
-                user: {
-                    id: 'any_id',
-                    email: 'alexfstos@gmail.com',
-                    name: 'Alex',
-                    photo: 'any_photo.png'
-                }
-            }
-            
-        }
-    }
-})
+jest.mock('expo-google-app-auth')
 
 describe('Auth Hook', () => {
     it('should be able to sign in with Google account existing',async () => {
+
+        const googleMocked = mocked(logInAsync as any)
+        googleMocked.mockReturnValue({
+            type: 'success',
+            user: {
+                id: 'any_id',
+                email: 'alexfstos@gmail.com',
+                name: 'Alex',
+                photo: 'any_photo.png'
+            }
+        })
+
         const { result } = renderHook(() => useAuth(), {
             wrapper: AuthProvider
         })
@@ -28,5 +27,21 @@ describe('Auth Hook', () => {
         
         expect(result.current.user.email)
         .toBe('alexfstos@gmail.com')
+    })
+
+    it('should not connect if user cancel authentication with Google',async () => {
+
+        const googleMocked = mocked(logInAsync as any)
+        googleMocked.mockReturnValue({
+            type: 'cancel',
+        })
+
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider
+        })
+
+        await act( () => result.current.signInWithGoogle())
+        
+        expect(result.current.user).not.toHaveProperty('id')
     })
 })
