8fa412d22e9c4e854031fa7bb4b33a938b709a41
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-02 13:09:36 -0300

    using mocks

--- ReactNative/gofinances/src/hooks/auth.test.tsx
+++ ReactNative/gofinances/src/hooks/auth.test.tsx
@@ -1,6 +1,23 @@
 import { renderHook, act} from '@testing-library/react-hooks'
 import {AuthProvider,useAuth} from './auth'
 
+jest.mock('expo-google-app-auth', ()=>{
+    return {
+        logInAsync: () => {
+            return{
+                type: 'success',
+                user: {
+                    id: 'any_id',
+                    email: 'alexfstos@gmail.com',
+                    name: 'Alex',
+                    photo: 'any_photo.png'
+                }
+            }
+            
+        }
+    }
+})
+
 describe('Auth Hook', () => {
     it('should be able to sign in with Google account existing',async () => {
         const { result } = renderHook(() => useAuth(), {
@@ -9,6 +26,7 @@ describe('Auth Hook', () => {
 
         await act( () => result.current.signInWithGoogle())
         
-        expect(result.current.user).toBeTruthy()
+        expect(result.current.user.email)
+        .toBe('alexfstos@gmail.com')
     })
 })
