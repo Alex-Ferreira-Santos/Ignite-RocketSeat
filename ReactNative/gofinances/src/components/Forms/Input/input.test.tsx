3152d4b45d3f481060ba7c1d2b87730be3d27fc9
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-02 11:13:00 -0300

    context test

--- ReactNative/gofinances/src/components/Forms/Input/input.test.tsx
+++ ReactNative/gofinances/src/components/Forms/Input/input.test.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react-native'
 import { Input } from '.';
+import { ThemeProvider } from 'styled-components/native'
+import theme from '../../../global/styles/theme'
+
+const Providers: React.FC = ({children}) => (
+    <ThemeProvider theme={theme}>
+        {children}
+    </ThemeProvider>
+)
 
 describe('Input Component',()=>{
     it('must have specific border color when active',()=>{
@@ -11,11 +19,14 @@ describe('Input Component',()=>{
                 keyboardType='email-address' 
                 autoCorrect={false} 
                 active={true}
-            />
+            />,
+            {
+                wrapper: Providers
+            }
         )
 
         const inputComponent = getByTestId('input-email')
-        expect(inputComponent.props.style[0].borderColor).toEqual('#f83f5b')
-        //expect(inputComponent.props.style[0].borderWidth).toEqual(3)
+        expect(inputComponent.props.style[0].borderColor).toEqual(theme.colors.attention)
+        expect(inputComponent.props.style[0].borderWidth).toEqual(3)
     })
 })
