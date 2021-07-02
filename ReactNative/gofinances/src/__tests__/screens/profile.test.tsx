f3d4e67346f03705842c5a9117ec287f16821220
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-02 10:01:46 -0300

    selectors

--- ReactNative/gofinances/src/__tests__/screens/profile.test.tsx
+++ ReactNative/gofinances/src/__tests__/screens/profile.test.tsx
@@ -3,10 +3,26 @@ import {render} from '@testing-library/react-native'
 
 import {Profile} from '../../screens/Profile'
 
-test('check if show correctly user input name placeholder', () => {
+test('checks if show correctly user input name placeholder', () => {
     const { getByPlaceholderText } = render(<Profile/>)
     const inputName = getByPlaceholderText('nome')
     
     
     expect(inputName).toBeTruthy()
+})
+
+test('checks if user data has benn loaded',()=>{
+    const { getByTestId } = render(<Profile/>)
+    const inputName = getByTestId('input-name')
+    const inputSurname = getByTestId('input-surname')
+
+    expect(inputName.props.value).toEqual('Alex')
+    expect(inputSurname.props.value).toEqual('Ferreira')
+})
+
+test('checks if title render correctly', () => {
+    const { getByTestId } = render(<Profile/>)
+    const textTitle = getByTestId('text-title')
+
+    expect(textTitle.props.children).toContain('Perfil')
 })
--- ReactNative/gofinances/src/screens/Profile/index.tsx
+++ ReactNative/gofinances/src/screens/Profile/index.tsx
@@ -4,15 +4,19 @@ import {View, Text, TextInput, Button} from 'react-native'
 export function Profile(){
     return(
         <View>
-            <Text>Perfil</Text>
+            <Text testID='text-title'>Perfil</Text>
 
             <TextInput
+                testID='input-name'
                 placeholder="nome"
                 autoCorrect={false}
+                value='Alex'
             />
 
             <TextInput
+                testID='input-surname'
                 placeholder="sobrenome"
+                value='Ferreira'
             />
 
             <Button title='Salvar' onPress={()=>{}}/>
