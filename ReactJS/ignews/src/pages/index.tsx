474581076f258919646fb78726af6617364ec6a4
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-08 14:40:36 -0300

    subscribe button component

--- ReactJS/ignews/src/pages/index.tsx
+++ ReactJS/ignews/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head'
+import SubscribeButton from '../components/SubscribeButton'
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head><title>Home | ig.news</title></Head>
-      <h1>Hello World</h1>
-
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
@@ -15,6 +14,7 @@ export default function Home() {
             Get acess to all the publications <br/>
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding "/>
