0edc87a4bbf04630df1265a3f7f197daae933380
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-12 14:00:53 -0300

    form data validation

--- ReactJS/dashgo/src/pages/index.tsx
+++ ReactJS/dashgo/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import React from 'react'
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
 import { Input } from '../components/Form/Input'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
@@ -8,8 +10,15 @@ type SignInFormData = {
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
-  const {register, handleSubmit,formState} = useForm()
+  const {register, handleSubmit,formState} = useForm({resolver: yupResolver(signInFormSchema)})
+
+  const {errors} = formState
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise((resolve, reject) => setTimeout(resolve,2000))
@@ -39,12 +48,14 @@ export default function SignIn() {
               name='email' 
               type='email' 
               label='E-mail'
+              error={errors.email}
               {...register('email')}
             />
             <Input 
               name='password' 
               type='password'
               label='Senha'
+              error={errors.password}
               {...register('password')}
             />
           
