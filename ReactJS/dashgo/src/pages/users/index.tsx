f5c5e77739737903ad3d306234ff1bb311afe8e3
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-12 15:21:50 -0300

    react query config

--- ReactJS/dashgo/src/pages/users/index.tsx
+++ ReactJS/dashgo/src/pages/users/index.tsx
@@ -1,19 +1,29 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine} from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import Link from 'next/link'
 import { useEffect } from "react";
+import { useQuery } from 'react-query'
 
 export default function UserList(){
+
+    const {data, isLoading, error} = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data
+    })
+
+
     const isWideVersion = useBreakpointValue({
         base:false,
         lg: true,
     })
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/users').then( response => response.json()).then( data => console.log(data))
+        
     },[])
 
     return(
@@ -42,67 +52,79 @@ export default function UserList(){
                         
                     </Flex>
 
-                    <Table colorScheme='whiteAlpha'>
-                        <Thead>
-                            <Tr>
-                                <Th px={['4','4','6']} color='gray.300' width='8'>
-                                    <Checkbox colorScheme='pink'/>
-                                </Th>
-                                <Th>Usuário</Th>
-                                { isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width='8'></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={['4','4','6']}>
-                                    <Checkbox colorScheme='pink'/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Alex Ferreira</Text>
-                                        <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={['4','4','6']}>
-                                    <Checkbox colorScheme='pink'/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Alex Ferreira</Text>
-                                        <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={['4','4','6']}>
-                                    <Checkbox colorScheme='pink'/>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Alex Ferreira</Text>
-                                        <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                <Td>
-                                
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                    {isLoading ? (
+                        <Flex justify='center'>
+                            <Spinner/>
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify='center'>
+                            <Text>Falha ao obter dados dos usuários</Text>
+                        </Flex>
+                    ) : (
+                    <>
+                        <Table colorScheme='whiteAlpha'>
+                            <Thead>
+                                <Tr>
+                                    <Th px={['4','4','6']} color='gray.300' width='8'>
+                                        <Checkbox colorScheme='pink'/>
+                                    </Th>
+                                    <Th>Usuário</Th>
+                                    { isWideVersion && <Th>Data de cadastro</Th>}
+                                    <Th width='8'></Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                <Tr>
+                                    <Td px={['4','4','6']}>
+                                        <Checkbox colorScheme='pink'/>
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight='bold'>Alex Ferreira</Text>
+                                            <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
+                                        </Box>
+                                    </Td>
+                                    { isWideVersion && <Td>04 de Abril, 2021</Td>}
+                                    <Td>
+                                    
+                                    </Td>
+                                </Tr>
+                                <Tr>
+                                    <Td px={['4','4','6']}>
+                                        <Checkbox colorScheme='pink'/>
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight='bold'>Alex Ferreira</Text>
+                                            <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
+                                        </Box>
+                                    </Td>
+                                    { isWideVersion && <Td>04 de Abril, 2021</Td>}
+                                    <Td>
+                                    
+                                    </Td>
+                                </Tr>
+                                <Tr>
+                                    <Td px={['4','4','6']}>
+                                        <Checkbox colorScheme='pink'/>
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight='bold'>Alex Ferreira</Text>
+                                            <Text fontSize='sm' color='gray.300'>alexfstos@gmail.com</Text>
+                                        </Box>
+                                    </Td>
+                                    { isWideVersion && <Td>04 de Abril, 2021</Td>}
+                                    <Td>
+                                    
+                                    </Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
 
-                    <Pagination/>
+                        <Pagination/>
+                    </>
+                    )}
                 </Box>
             </Flex>
         </Box>
