138b4d8cb461c31f8f9d2bad87610f2e6edbbdd3
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-12 17:18:41 -0300

    data prefetch

--- ReactJS/dashgo/src/pages/users/index.tsx
+++ ReactJS/dashgo/src/pages/users/index.tsx
@@ -1,20 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
 import { RiAddLine} from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList(){
+    const [page,setPage] = useState(1)
 
-    const {data, isLoading, isFetching, error} = useUsers()
+    const {data, isLoading, isFetching, error} = useUsers(page)
 
 
     const isWideVersion = useBreakpointValue({
         base:false,
         lg: true,
     })
+
+    async function handlePrefetchUser(userId:number){
+        await queryClient.prefetchQuery(['user',userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data
+        },{
+            staleTime: 1000 * 60 * 10, // 10 minutos
+        })
+    }
 
     return(
         <Box>
@@ -31,7 +45,7 @@ export default function UserList(){
                             {!isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
                         </Heading>
 
-                        <Link href='/users/create' passHref>
+                        <NextLink href='/users/create' passHref>
                             <Button
                                 as='a'
                                 size='sm'
@@ -41,7 +55,7 @@ export default function UserList(){
                             >
                                 Criar novo 
                             </Button>
-                        </Link>
+                        </NextLink>
 
                         
                     </Flex>
@@ -83,7 +97,7 @@ export default function UserList(){
                                     
                                     </Td>
                                 </Tr>
-                                {data.map( user => {
+                                {data.users.map( user => {
                                     return(
                                         <Tr key={user.id}>
                                             <Td px={['4','4','6']}>
@@ -91,7 +105,9 @@ export default function UserList(){
                                             </Td>
                                             <Td>
                                                 <Box>
-                                                    <Text fontWeight='bold'>{user.name}</Text>
+                                                   <Link color='purple.400' onMouseEnter={() => handlePrefetchUser(Number(user.id))}>
+                                                        <Text fontWeight='bold'>{user.name}</Text>
+                                                    </Link>
                                                     <Text fontSize='sm' color='gray.300'>{user.email}</Text>
                                                 </Box>
                                             </Td>
@@ -106,9 +122,9 @@ export default function UserList(){
                         </Table>
 
                         <Pagination
-                            totalCountOfRegisters={200}
-                            currentPage={5}
-                            onPageChange={() => {}}
+                            totalCountOfRegisters={data.totalCount}
+                            currentPage={page}
+                            onPageChange={setPage}
                         />
                     </>
                     )}
