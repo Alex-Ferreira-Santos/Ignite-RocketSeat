833ee3051d5768518f6fe21e73a041a4cdf68132
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-13 10:32:55 -0300

    log out

--- ReactJS/nextauth/context/AuthContext.tsx
+++ ReactJS/nextauth/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies'
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
 import { api } from "../services/api";
 import Router from 'next/router'
 
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut(){
+    destroyCookie(undefined, 'nextauth.token')
+    destroyCookie(undefined, 'nextauth.refreshtoken')
+
+    Router.push('/')
+}
+
 export function AuthProvider({children}: AuthProviderProps){
 
     const [user,setUser] = useState<User>(null)
@@ -39,7 +46,7 @@ export function AuthProvider({children}: AuthProviderProps){
                 const { email, permissions, roles} = response.data
 
                 setUser({ email, permissions, roles })
-            })
+            }).catch(() => signOut())
         }
 
     },[])
