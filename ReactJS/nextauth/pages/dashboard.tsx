9dc0899afb732dd35609ca5be4300b69e2dc7b29
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-13 14:36:37 -0300

    log out broadcast

--- ReactJS/nextauth/pages/dashboard.tsx
+++ ReactJS/nextauth/pages/dashboard.tsx
@@ -1,16 +1,13 @@
-import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { Can } from "../components/Can"
 import { AuthContext } from "../context/AuthContext"
-import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
-import { AuthTokenError } from "../services/errors/AuthTokenError"
 
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
-    const { user, isAuthenticated } = useContext(AuthContext)
+    const { user, isAuthenticated, signOut } = useContext(AuthContext)
 
     useEffect(()=>{
         api.get('/me')
@@ -21,6 +18,8 @@ export default function Dashboard(){
     return(
         <div>
             <h1>Dashboard: {user?.email}</h1>
+
+            <button onClick={signOut}>Sign out</button>
 
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
