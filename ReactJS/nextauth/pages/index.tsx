3283b69787d8973b3d9c7449f24482f128269b15
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-13 11:50:05 -0300

    server validation

--- ReactJS/nextauth/pages/index.tsx
+++ ReactJS/nextauth/pages/index.tsx
@@ -1,8 +1,7 @@
-import { GetServerSideProps } from "next"
 import { FormEvent, useContext, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
 import styles from '../styles/Home.module.css'
-import { parseCookies } from 'nookies'
+import { withSSRGuest } from "../utils/withSSRGuest"
 
 export default function Home() {
   const [email,setEmail] = useState('')
@@ -30,19 +29,10 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return{
+    props:{
 
-  if(cookies['nextauth.token']){
-    return{
-      redirect:{
-        destination: '/dashboard',
-        permanent: false
-      }
     }
   }
-
-  return{
-    props:{}
-  }
-}
+})
